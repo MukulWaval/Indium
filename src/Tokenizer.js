af2254360715a29d50736b01591527790812b449
MukulWaval <mukulwaval2000@gmail.com>
2024-10-24 21:15:26 +0530

    Add whitespace token

---
+++ src/Tokenizer.js
@@ -0,0 +1,87 @@
+/**
+ * Tokenizer spec.
+ */
+const Spec = [
+  // WHITESPACE--------------------------------------------------------
+  [/^\s+/, null],
+];
+
+/**
+ * Tokenizer class.
+ *
+ * Lazily pulls a token from a stream.
+ */
+class Tokenizer {
+  /**
+   * Initialize the tokenizer.
+   */
+  constructor() {
+    this._cursor = 0;
+    this._string = "";
+  }
+
+  /**
+   * Initialize the string.
+   */
+  init(string) {
+    this._string = string;
+    this._cursor = 0;
+  }
+
+  /**
+   * Whether tokenizer reaches EOF.
+   */
+  isEOF() {
+    return this._cursor === this._string.length;
+  }
+
+  /**
+   * Whether we still have tokens.
+   */
+  hasMoreTokens() {
+    return this._cursor < this._string.length;
+  }
+
+  /**
+   * Obtains the next token.
+   */
+  getNextToken() {
+    if (!this.hasMoreTokens()) {
+      return null;
+    }
+
+    const string = this._string.slice(this._cursor);
+
+    for (const [regex, tokenType] of Spec) {
+      const tokenValue = this._matched(regex, string);
+      // Can't match this rule, continue.
+      if (tokenValue == null) {
+        continue;
+      }
+
+      // Skip token, e.g. whitespace.
+      if (tokenType == null) {
+        return this.getNextToken();
+      }
+
+      return {
+        type: tokenType,
+        value: tokenValue,
+      };
+    }
+
+    throw new SyntaxError(`Unexpected token: "${string[0]}"`);
+  }
+
+  /**
+   * Matches a token for a regular expression.
+   */
+  _matched(regex, string) {
+    const matched = regex.exec(string);
+    if (matched == null) {
+      return null;
+    }
+    this._cursor += matched[0].length;
+    return matched[0];
+  }
+}
