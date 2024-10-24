694ce13390fc009dd70d7c4e97867cf819bdc6e6
MukulWaval <mukulwaval2000@gmail.com>
2024-10-24 21:31:25 +0530

    add conditional statements

--- src/Parser.js
+++ src/Parser.js
@@ -245,6 +245,30 @@ class Parser {
   }
 
   /**
+   * IfStatement
+   *  : 'if' '(' Expression ')' Statement
+   *  | 'if' '(' Expression ')' Statement 'else' Statement
+   *  ;
+   */
+  IfStatement() {
+    this._eat("if");
+    this._eat("(");
+    const test = this.Expression();
+    this._eat(")");
+    const consequent = this.Statement();
+    const alternate =
+      this._lookahead != null && this._lookahead.type === "else"
+        ? this._eat("else") && this.Statement()
+        : null;
+    return {
+      type: "IfStatement",
+      test,
+      consequent,
+      alternate,
+    };
+  }
+
+  /**
    * Expects a token of a given type.
    */
   _eat(tokenType) {
