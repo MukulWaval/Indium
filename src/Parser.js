5ef7b9165a9c92e10b82d9f03e6d8f7cfd8d54e9
MukulWaval <mukulwaval2000@gmail.com>
2024-10-24 21:28:11 +0530

    Add program

--- src/Parser.js
+++ src/Parser.js
@@ -1,39 +1,68 @@
 /**
  * Letter parser: recursive descent implementation.
  */
-const {Tokenizer} = require('../src/Tokenizer');
+const { Tokenizer } = require("../src/Tokenizer");
 
 class Parser {
-    /**
-     * Initialize the parser.
-     */
-    constructor() {
-        this._string = '';
-        this._tokenizer = new Tokenizer();
-    }
+  /**
+   * Initialize the parser.
+   */
+  constructor() {
+    this._string = "";
+    this._tokenizer = new Tokenizer();
+  }
 
-    /**
-     * Expects a token of a given type.
-     */
-    _eat(tokenType){
-        const token = this._lookahead;
-        if (token == null)
-        {
-            throw new SyntaxError(
-                `Unexpected end of input, expected: "${tokenType}"`,
-            );
-        }
-        if (token.type !== tokenType) {
-            throw new SyntaxError(
-                `Unexpected token: "${token.value}",` +
-                `expected "${tokenType}"`,
-            );
-        }
-        // Advance to next token
-        this._lookahead = this._tokenizer.getNextToken();
-        return token;
+  /**
+   * Parses a string into AST.
+   */
+  parse(string) {
+    this._string = string;
+    this._tokenizer.init(string);
+
+    // Prime the tokenizer to obtain the first
+    // token which is our lookahead. The lookahead is
+    // used for predictive parsing.
+
+    this._lookahead = this._tokenizer.getNextToken();
+
+    // Parse recursively from the main
+    // entry point , the Program:
+
+    return this.Program();
+  }
+
+  /**
+   * Program
+   *  : StatementList
+   *  ;
+   */
+  Program() {
+    return {
+      type: "Program",
+      body: this.StatementList(),
+    };
+  }
+
+  /**
+   * Expects a token of a given type.
+   */
+  _eat(tokenType) {
+    const token = this._lookahead;
+    if (token == null) {
+      throw new SyntaxError(
+        `Unexpected end of input, expected: "${tokenType}"`
+      );
     }
+    if (token.type !== tokenType) {
+      throw new SyntaxError(
+        `Unexpected token: "${token.value}",` + `expected "${tokenType}"`
+      );
+    }
+    // Advance to next token
+    this._lookahead = this._tokenizer.getNextToken();
+    return token;
+  }
 }
 module.exports = {
-    Parser,
-}
+  Parser,
+};
